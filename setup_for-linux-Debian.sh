596b03a793ae3d7c4d999721458db190f04bd5b7
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2023-10-11 22:26:24 +0900

    Add_shell-script_libraly-version

--- setup_for-linux-Debian.sh
+++ setup_for-linux-Debian.sh
@@ -8,6 +8,6 @@ sudo apt-get install python3-pip
 # ライブラリをインストール
 pip3 install discord.py
 pip3 install python-dotenv
-pip3 install Pillow
+pip3 install Pillow==10.0.1
 pip3 install pyyaml
 pip3 install requests
